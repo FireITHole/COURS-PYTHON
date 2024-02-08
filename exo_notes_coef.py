fddafca6bc941d7e793cbf7a9056e904a1871031
aarnoux <aarnoux@bt-formation.fr>
2024-02-08 16:28:04 +0100

    correction exo note coef

--- exo_notes_coef.py
+++ exo_notes_coef.py
@@ -1,2 +1,11 @@
 notes = [{"note": 10, "coef": 2}, {"note": 12, "coef": 3}, {"note": 16, "coef": 4}]
-# Trouver la moyenne de ces notes en prenant en compte leur coéfficient
+# Trouver la moyenne de ces notes en prenant en compte leur coéfficient (moyenne pondérée)
+
+nombre_notes = 0
+total_notes = 0
+
+for note in notes:
+    nombre_notes += note["coef"]
+    total_notes += note["note"]*note["coef"]
+
+print(total_notes/nombre_notes)
